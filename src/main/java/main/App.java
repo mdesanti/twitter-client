8692241504b87d475721ed72044bad8d963aa319
Matias De Santi <mdesanti90@gmail.com>
2013-11-03 16:24:30 -0300

    Now saves in file

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,44 +1,67 @@
 package main;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.google.common.collect.Lists;
 import com.twitter.hbc.core.Client;
 
 public class App {
-	
+
 	private static Charset CHARSET = Charset.forName("ISO-8859-1");
+	private static int filenr = 0;
+	private static Logger logger = Logger.getLogger("twitter-client");
 
 	public static void main(String[] args) throws InterruptedException {
+
 		final BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
-		final Client client = new TwitterClientBuilder()
-								.withKeysInFile("twitter-keys.properties")
-								.withQueue(queue)
-								.tracking("#Elecciones2013")
-								.build();
-		
+		final Client client = new TwitterClientBuilder().withKeysInFile("twitter-keys.properties").withQueue(queue).tracking("#PonelePeteALaPelicula").build();
+
 		client.connect();
 
-		// Do whatever needs to be done with messages
 		Thread t = new Thread(new Runnable() {
 
 			public void run() {
 				while (!client.isDone()) {
-					String msg;
+					// wait 5 seconds for the queue to get some tweets
 					try {
-						msg = queue.take();
-						JSONObject json = (JSONObject)new JSONParser().parse(msg);
-						String text = new String(((String)json.get("text")).getBytes(), CHARSET);
-						System.out.println(msg);
-					} catch (InterruptedException e) {
-						//interrupted!
+						Thread.sleep(5000);
+						List<String> tweets = Lists.newArrayList();
+						queue.drainTo(tweets, 50);
+						if(!tweets.isEmpty()) {
+							PrintWriter writer = new PrintWriter("tweets/tweets" + filenr + ".txt", "UTF-8");
+							for (String str : tweets) {
+								JSONObject json = (JSONObject) new JSONParser().parse(str);
+								String text = new String(((String) json.get("text")).getBytes(), CHARSET);
+								writer.println(text);
+							}
+							logger.log(Level.INFO, "Wrote file tweets" + filenr + ".txt");
+							filenr++;
+							writer.close();
+						}
 					} catch (ParseException e) {
-						//error!
+						// error!
+						e.printStackTrace();
+					} catch (FileNotFoundException e) {
+						// error!
+						e.printStackTrace();
+					} catch (UnsupportedEncodingException e) {
+						// error!
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						// error!
+						e.printStackTrace();
 					}
 				}
 			}
@@ -48,5 +71,4 @@ public class App {
 		client.stop();
 
 	}
-
 }
